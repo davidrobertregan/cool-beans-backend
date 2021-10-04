18b3cb2a6999f276075334ada4799fde70a0c3e3
Nicholas A Christensen <nickachristensen@gmail.com>
2021-10-04 17:12:20 -0400

    Coffee controller get experiment

---
+++ app/controllers/coffee_controller.rb
@@ -0,0 +1,8 @@
+class CoffeeController < ApplicationController
+    set :default_content_type, 'application/json'
+    
+    # Add your routes here
+    get "/coffees" do
+        Coffee.all.to_json(include: [:roaster, { reviews: { include: :drinker } }])
+    end
+  end
